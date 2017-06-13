9f7b62c50feb35bbf13b31cdc6e607be7aed40a0
Koji Murata <koji.murata@dena.com>
2017-06-13 15:23:09 +0900

    ignore

---
+++ Example/Pods/Target Support Files/Pods-Balblair_Tests/Pods-Balblair_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Balblair_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Balblair_Tests
+@end
---
+++ Example/Pods/Target Support Files/Pods-Balblair_Tests/Pods-Balblair_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Balblair_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Balblair_TestsVersionString[];
+
