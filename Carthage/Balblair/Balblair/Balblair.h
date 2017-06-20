596b31cbe70aa69d903702bb5ce2d0aaf172f10d
Koji Murata <koji.murata@dena.com>
2017-06-20 20:47:44 +0900

    add project

---
+++ Carthage/Balblair/Balblair/Balblair.h
@@ -0,0 +1,19 @@
+//
+//  Balblair.h
+//  Balblair
+//
+//  Created by Koji Murata on 2017/06/20.
+//  Copyright © 2017年 Koji Murata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Balblair.
+FOUNDATION_EXPORT double BalblairVersionNumber;
+
+//! Project version string for Balblair.
+FOUNDATION_EXPORT const unsigned char BalblairVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Balblair/PublicHeader.h>
+
+
