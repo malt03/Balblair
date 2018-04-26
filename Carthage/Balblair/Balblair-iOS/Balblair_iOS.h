5604c6c7c82542acc3b4cb8d3eecc38f0ecb3fe7
Koji Murata <koji.murata@dena.com>
2018-04-27 01:15:31 +0900

    mac carthage

---
+++ Carthage/Balblair/Balblair-iOS/Balblair_iOS.h
@@ -0,0 +1,18 @@
+//
+//  Balblair_iOS.h
+//  Balblair-iOS
+//
+//  Created by Koji Murata on 2018/04/27.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Balblair_iOS.
+FOUNDATION_EXPORT double Balblair_iOSVersionNumber;
+
+//! Project version string for Balblair_iOS.
+FOUNDATION_EXPORT const unsigned char Balblair_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Balblair_iOS/PublicHeader.h>
+
+
---
+++ Carthage/Balblair/Balblair-macOS/Balblair_macOS.h
@@ -0,0 +1,18 @@
+//
+//  Balblair_macOS.h
+//  Balblair-macOS
+//
+//  Created by Koji Murata on 2018/04/27.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Balblair_macOS.
+FOUNDATION_EXPORT double Balblair_macOSVersionNumber;
+
+//! Project version string for Balblair_macOS.
+FOUNDATION_EXPORT const unsigned char Balblair_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Balblair_macOS/PublicHeader.h>
+
+
